9721dc82e5699a574d46241eb53a4f4dc19b564a
asokpavi <208320240+asokpavi@users.noreply.github.com>
2025-04-22 01:21:27 -0400

    Update PRD.py

--- Final_Version/PRD.py
+++ Final_Version/PRD.py
@@ -275,7 +275,7 @@ runnable_graph = graph.compile()
 
 #title and description
 
-st.title("📑PRD Mastermind: AI Agent-Driven Requirements & Risk Engine")
+st.title("📑PRD - AI Agent Mastermind")
 
 # Collect user inputs
 product_name = st.text_input("Product Name")
